f157f4e54dbf27e5319f07ac6be45455d6b3163c
Arsenii6666 <92683180+Arsenii6666@users.noreply.github.com>
2021-12-15 10:22:52 +0200

    Create Placing ship.py

---
+++ Placing ship.py
@@ -0,0 +1,5 @@
+def main():
+    Ship_lst=['****','***','***','**','**','**','*','*','*','*']
+    print("To place ship print coordinate of left position. If you need to place it horizontaly, print 0.")
+    while True:
+        print("Your ship:")
